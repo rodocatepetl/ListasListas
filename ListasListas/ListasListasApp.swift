6e0c31074dcc92989a1b411bfe04d8750be48294
Rodolfo Castillo <raindeerdeveloper+dev@gmail.com>
2024-02-01 16:18:43 -0600

    Initial Commit

---
+++ ListasListas/ListasListasApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListasListasApp.swift
+//  ListasListas
+//
+//  Created by Rodolfo Castillo on 01/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ListasListasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
